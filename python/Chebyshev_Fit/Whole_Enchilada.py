9dbe8986bc96e3414ccb7076f63bfe89437e48d7
JDFlorezCastillo <jd.florezcastillo@gmail.com>
2021-09-23 01:33:33 -0400

    Updated unit tests, plotting ABCD

--- python/Chebyshev_Fit/Whole_Enchilada.py
+++ python/Chebyshev_Fit/Whole_Enchilada.py
@@ -36,134 +36,70 @@ class alphabet_skills:
             alphabet ([.npz]): [a bunch of .npy files]
         """
         data = np.load('all_letters.npz', allow_pickle=True)
-
+        traj = []
         # data_a is a list of strokes, where each stroke is an Nx2 numpy array
         data_a = data['A']
         strokea1 = data_a[1]
         strokea2 = data_a[3]
-        plt.plot(strokea1[:, 0], strokea1[:, 1])
-        plt.plot(strokea2[:, 0], strokea2[:, 1])
-        plt.show()
+        # plt.plot(strokea1[:, 0], strokea1[:, 1])
+        # plt.plot(strokea2[:, 0], strokea2[:, 1])
+        # plt.show()
 
         data_b = data['B']
         strokeb1 = data_b[1]
         strokeb2 = data_b[3]
         strokeb3 = data_b[5]
-        # plt.plot(strokeb1[:, 0], strokeb1[:, 1])
-        # plt.plot(strokeb2[:, 0], strokeb2[:, 1])
-        # plt.plot(strokeb3[:, 0], strokeb3[:, 1])
-        # plt.show()
 
         data_c = data['C']
         strokec1 = data_c[1]
-        # for stroke in data_c:
-        #     plt.plot(stroke[:, 0], stroke[:, 1])
-        # plt.show()
+
         data_d = data['D']
         stroked1 = data_d[1]
         stroked2 = data_d[3]
-        for stroke in data_d:
-            plt.plot(stroke[:, 0], stroke[:, 1])
-        plt.show()
-        data_e = data['E']
-        # for stroke in data_e:
-        #     plt.plot(stroke[:, 0], stroke[:, 1])
-        # plt.show()
-        data_f = data['F']
-        # for stroke in data_f:
-        #     plt.plot(stroke[:, 0], stroke[:, 1])
-        # plt.show()
-        # trajectory = np.empty(len(input_sequence))
-        # index = 0
-        # print(data_a.shape)
-        # for element in input_sequence:
-        #     if element == "A":
-        #         trajectory[index] = data_a
-        #     elif element == "B":
-        #         trajectory[index] = data_b
-        #     elif element == "C":
-        #         trajectory[index] = data_c
-        #     elif element == "D":
-        #         trajectory[index] = data_d
-        #     elif element == "E":
-        #         trajectory[index] = data_e
-        #     elif element == "F":
-        #         trajectory[index] = data_f
-        #     else:
-        #         print("Invalid desired trajectory")
-        #     index += 1
-        traj = np.array([[strokea1, strokea2], [strokeb1, strokeb2, strokeb3],
-                        [strokec1], [stroked1, stroked2]])
+
+        traj = []
+        for input in input_sequence:
+            if input == "A":
+                traj.append(np.array([strokea1, strokea2]))
+            if input == "B":
+                traj.append(np.array([strokeb1, strokeb2, strokeb3]))
+            if input == "Test":  # plot basic test case
+                traj = np.array([[strokea1, strokea2],
+                                [strokeb1, strokeb2, strokeb3],
+                                [strokec1], [stroked1, stroked2]])
         return(traj)
 
-    def Chebyshev_fit(self, ):  # , read_2D: capture_trajectory):
-        """Process the input and fit to Chebyshev using GTSAM"""
-        # Call the GTSAM functions here
-
-    def test_fit_basis_chebyshev1basis(self, data, time, p_order):
-        """Fits a Chebyshev Basis and interpolates for x or y against t
+    def Chebyshev_fit(self, data, time, p_order):
+        """
+        Fits a Chebyshev Basis and interpolates for x against t using GTSAM
 
         Args:
             data ([array]): [the x or y coordinates of a stroke]
+            time ([array]): [the time for each x or y position]
+            porder ([int]): [the order of chebyshev polynomial]
         """
-        # time = data[0]
-        # sample = data[1]
         noise = gtsam.noiseModel.Unit.Create(1)
         self.x = [0., 0.5, 0.75]
         fit = gtsam.FitBasisChebyshev1Basis(data, noise, p_order)
         coeff = fit.parameters()
-        print("COEFF", coeff)
         basis = gtsam.Chebyshev1Basis
         interp = basis.WeightMatrix(p_order, time) @ coeff
         return(interp)
 
+    def render_trajectory(self, traj, order):
+        """
+        Render trajectory that reproduces artistic intent.
 
-    def render_trajectory(self):  # , Cheby: Chebyshev_fit):
-        """Render trajectory that reproduces artistic intent."""
-        return("""List of list of polynomials""")
-
-    def generate_figure(self):  # , Cheby: Chebyshev_fit, render: render_trajectory):
-        """Create results figure to compare trajectory with fit."""
-        # Jupyter Notebook will call on this function for plotting.
-
-
-class TestWholeEnchilada(GtsamTestCase):
-    """Test end-to-end for art skills project."""
-
-    # def test_V_trajectory(self):
-    #     """Generate a trajectory."""
-    #     skills = alphabet_skills()
-    #     letters = ["V"]
-    #     stepsize = 0.02
-    #     trajectory = skills.create_V(letters, stepsize)
-    #     x = trajectory[0][0]
-    #     y = trajectory[0][1]
-    #     plt.plot(x, y)
-    #     plt.show()
-
-    def test_capture_trajectory(self):
-        """Generate a trajectory."""
-        skills = alphabet_skills()
-        trajectory = skills.capture_trajectory(["A"])
-        return(trajectory)
-
-    def test_chebyshev(self):
-        skills = alphabet_skills()
-        
-        # traj_x = []
-        # traj_y = []
-        # traj_t = []
-        # data_x = []
-        # data_y = []
+        Args:
+            traj ([array]): [the letters and strokes of each letter]
+            order ([int]): [the order of chebyshev polynomial]
+        """
         interp_x = []
         interp_y = []
         count_s = 0
         count_l = 0
-        order = 12
-        traj = self.test_capture_trajectory()
         for letter in traj:
             for stroke in letter:
-                print("LETTER", letter)
                 traj_x = (stroke[:, 0] + count_l*0.7)
                 traj_y = (stroke[:, 1])
                 traj_t = linspace(0, 1, len(traj_x))
@@ -171,33 +107,47 @@ class TestWholeEnchilada(GtsamTestCase):
                 zipy = zip(traj_t, traj_y)
                 data_x = dict(zipx)
                 data_y = dict(zipy)
-                interp_x.append(skills.test_fit_basis_chebyshev1basis(data_x, traj_t, order))
-                interp_y.append(skills.test_fit_basis_chebyshev1basis(data_y, traj_t, order))
-                plt.plot(interp_x[count_s], interp_y[count_s])
+                interp_x.append(self.Chebyshev_fit(data_x, traj_t, order))
+                interp_y.append(self.Chebyshev_fit(data_y, traj_t, order))
                 count_s += 1
             count_l += 1
+        return(interp_x, interp_y)
+
+
+class TestWholeEnchilada(GtsamTestCase):
+    """Test end-to-end for Chebyshev fitting."""
+
+    def test_capture_trajectory(self):
+        """Generate a trajectory."""
+        skills = alphabet_skills()
+        trajectory = skills.capture_trajectory(["A"])
+        plt.plot(trajectory[0][0][:, 0], trajectory[0][0][:, 1])
+        plt.plot(trajectory[0][1][:, 0], trajectory[0][1][:, 1])
         plt.show()
 
-    # def test_write_stroke(self):
-    #     """Write a single stroke."""
-    #     return ("""One polynomial""")
+    def test_chebyshev(self):
+        """Test Chebyshev fitting"""
+        skills = alphabet_skills()
+        x = np.array([0, 0.1, 1.5, 0.25, 0])
+        t = np.array([0, 0.5,   1,  1.5, 2])
+        order = 3
+        zipx = zip(t, x)
+        datax = dict(zipx)
+        trajectory = skills.Chebyshev_fit(datax, t, order)
+        plt.plot(t, x)
+        plt.plot(t, trajectory)
+        plt.show()
 
-    # def test_write_letter(self):
-    #     """Write a single letter."""
-    #     return ("""list of polynomials describing one letter""")
-
-    # def test_write_letters(self):
-    #     """Write out a sequence of letters."""
-    #     letter = self.test_write_letter
-    #     return("""list of lists of polynomials""")
-
-    # def test_write_alphabet(self):
-    #     """Capture/take input, perceive/process, and render the entire
-    #     alphabet."""
-    #     skills = alphabet_skills()
-    #     captured_trajectory = skills.capture_trajectory()
-    #     cheby_out = skills.Chebyshev_fit(captured_trajectory)
-    #     skills.render_trajectory(cheby_out)
+    def test_write_alphabet(self):
+        """Capture/take input, perceive/process, and render the entire
+        alphabet."""
+        skills = alphabet_skills()
+        traj = skills.capture_trajectory(["Test"])
+        xcoord, ycoord = skills.render_trajectory(traj, 12)
+        for stroke in range(len(xcoord)):
+            plt.plot(xcoord[stroke], ycoord[stroke])
+        plt.axis("equal")
+        plt.show()
 
 
 if __name__ == "__main__":
