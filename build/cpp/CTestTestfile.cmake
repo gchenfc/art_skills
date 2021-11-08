f8fcb4304c33ec9b57c5942aee8d1641b37c9de5
Frank Dellaert <dellaert@gmail.com>
2021-11-08 11:16:12 -0500

    Delete folders that should not be checked in

--- build/cpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jdflo/GIT_REPOS/gtsam_projects/art_skills/cpp
-# Build directory: /home/jdflo/GIT_REPOS/gtsam_projects/art_skills/build/cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("SLM")
