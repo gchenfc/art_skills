48446b5a243b7ae0e9443aeb2d4c32374d3bf6f0
JD Florez <jdflorez.vca@gmail.com>
2021-11-16 17:52:53 -0500

    removed build folder from git

--- build/cpp/CMakeFiles/chebyshev_alphabet_test.run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/chebyshev_alphabet_test.run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/chebyshev_alphabet_test.run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/cpp/CMakeFiles/jd00_chebyshevAlphabet.run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/jd00_chebyshevAlphabet.run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/jd00_chebyshevAlphabet.run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
