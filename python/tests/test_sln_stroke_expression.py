9bff90d314b40b7967b422727a44525475fa8dbe
Gerry Chen <gerry.chen2015@gmail.com>
2022-03-04 09:43:06 -0500

    Unit test pos_integration_factor correctness

--- python/tests/test_sln_stroke_expression.py
+++ python/tests/test_sln_stroke_expression.py
@@ -10,6 +10,7 @@ Author: Frank Dellaert, Sang-Won Leigh, JD Florez-Castillo
 """
 
 import unittest
+import numpy as np
 import art_skills
 from art_skills import SlnStrokeExpression
 import gtsam
@@ -25,6 +26,36 @@ class TestSlnStrokeExpression(GtsamTestCase):
         params_key = P(1)
         stroke = SlnStrokeExpression(params_key)
         factor = stroke.pos_integration_factor(12345, 0.01)
+
+    def test_factor_correctness(self):
+        params_key = P(1)
+        stroke = SlnStrokeExpression(params_key)
+        dt = 0.01
+        x0 = np.array([0.00123, 0.00456])
+
+        # Create graph
+        graph = gtsam.NonlinearFactorGraph()
+        graph.add(stroke.pos_integration_factor(0, dt))
+
+        # Add priors
+        #                                    t0 D th1 th2 sigma mu
+        graph.addPriorVector(P(1), np.array([-2 + dt, 1, 0, 0, 1, 0]),
+                             gtsam.noiseModel.Constrained.All(6))
+        graph.addPriorPoint2(X(0), x0, gtsam.noiseModel.Constrained.All(2))
+
+        # Initialization
+        init = gtsam.Values()
+        init.insert(P(1), np.array([0, 1, 0, 0, 1, 0]))
+        init.insert(X(0), x0)
+        init.insert(X(1), np.zeros(2))
+
+        # Solve
+        sol = gtsam.LevenbergMarquardtOptimizer(graph, init).optimize()
+
+        expected = np.array([dt * 1 *
+                             (1 / np.sqrt(2 * np.pi) / 2) * np.exp(-np.log(2)**2 / 2), 0]) + x0
+        actual = sol.atPoint2(X(1))
+        self.gtsamAssertEquals(actual, expected)
 
 
 if __name__ == "__main__":
